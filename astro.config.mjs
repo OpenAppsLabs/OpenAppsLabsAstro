5e265823ae3f3d4752f8e5abfcdcdcd55bdb1098
Rajat <rajatpatil468@gmail.com>
2026-01-08 19:45:59 +0530

    fixed path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://openappslabs.github.io/OpenAppsLabsAstro',
-  base: '/OpenAppsLabsAstro',
+  site: 'https://openappslabs.github.io/OpenAppsLabs',
+  base: '/OpenAppsLabs',
 });
